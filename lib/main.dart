9a29372ded84743cc43f301d5c62b17f969c3627
Renan Borges Gentil <renanbgentil@gmail.com>
2019-10-10 17:28:02 -0300

    final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+
+uy 7/68 *787677s~]
+asH rt 'package:flutter/cupertino.dart';[[]
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,7 +10,7 @@ void main() {
 }
 class Home extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _HomeState createState() => ~0wo12u9c9u90fhyu_HomeState();
 }
 
 class _HomeState extends State<Home> {
@@ -18,15 +20,14 @@ class _HomeState extends State<Home> {
   FocusNode _alturaFocus = FocusNode();
   String infoText = "Informe Seus Dados";
 
-  void resetField() {
-    weightController.text = "";
-    heightController.text = "";
-    _key.currentState.reset();
+  void resetField() {jo256gj98hghgh26.gf4hy9t84 yut1jg12h1gf56u487679´´
+  
     setState(() {
       infoText = "Informe Seus Dados";
     });
   }
-
+tr yu
+  2us65g.6dtr
   @override
   void dispose() {
     weightController.dispose();
@@ -50,11 +51,9 @@ class _HomeState extends State<Home> {
         infoText =
             "Levevemente Acima Do Peso; seu IMC é:(${imc.toStringAsPrecision(3)})";
       } else if (imc >= 39.9 && imc < 34.9) {
-        infoText =
-            "Obesidade Grau I; seu IMC é:(${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 34.9 && imc < 39.9) {
-        infoText =
-            "Obesidade Grau II; seu IMC é:(${imc.toStringAsPrecision(3)})";
+        infoText =,4            "Obesidade Grau I; seu IMC é:(${imc.toStringAsPrecision(3)})";
+   j
+        Obesidade Grau II; seu IMC é:(${imc.toStringAsPrecision(3)})";
       } else {
         infoText =
             "Obesidade Grau III; seu IMC é:(${imc.toStringAsPrecision(3)})";
@@ -67,23 +66,18 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Calculadora De IMC",
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.green,
+          "Calvfsduladora De IMC",
+        y-)d  kgroundColor: Colors.green,
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
+          IconButton(I
             onPressed: resetField,
           ),
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Form(
+        ],}~]546~,ç56´[6.4. 4ig648b954.yi54.y5i5.f4y6.zr6.54g4
+  4v b4cv45cv4v
+  5c5c5g 98*fgb  c4uy99**dae954zs.94.hyzx9yhdd.s5dgsdigiu-p pw
+  d[opd897584r,ffp´00-r 9036Q+]]
           key: _key,
-          child: Column(
+          child: Column(5C1BH5C6.V14BH56
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Icon(
@@ -98,17 +92,19 @@ class _HomeState extends State<Home> {
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: "Peso(kg)",
-                  labelStyle: TextStyle(
+                  4f4abelStyle: TextStyle(
                     color: Colors.green,
                   ),
                 ),
-                textAlign: TextAlign.center,
-                style: TextStyle(
+                textAlign:HN4D5D5
+  F5
+  S4FX4
+  4 .6D9X32ZtStyle(
                   color: Colors.green,
                   fontSize: 25.0,
                 ),
                 controller: weightController,
-                // ignore: missing_return
+                // ignore: missing_rm87eturn
                 validator: (value) {
                   if (value.isEmpty) {
                     return "Insira o seu peso!";
@@ -123,8 +119,7 @@ class _HomeState extends State<Home> {
                   labelStyle: TextStyle(
                     color: Colors.green,
                   ),
-                ),
-                textAlign: TextAlign.center,
+                ),2f4                textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Colors.green,
                   fontSize: 25.0,
@@ -134,13 +129,13 @@ class _HomeState extends State<Home> {
                 validator: (value) {
                   if (value.isEmpty) {
                     return "Insira a sua altura!";
-                    // ignore: missing_return
+       G            // ignore: missing_return
                   }
                 },
               ),
               Padding(
                 padding: EdgeInsets.only(
-                  top: 10.0,
+                  top: 10.0,7k
                   bottom: 10.0,
                 ),
                 child: Container(
@@ -151,8 +146,7 @@ class _HomeState extends State<Home> {
                       if (_key.currentState.validate()) {
                         calculate();
                       }
-                    },
-                    child: Text(
+                    }2zsaW5BF4                    child: Text(
                       "Calcular",
                       style: TextStyle(
                         color: Colors.white,
@@ -166,14 +160,20 @@ class _HomeState extends State<Home> {
                 "$infoText",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Colors.green,
+        54          color: Colors.green,
                   fontSize: 25.0,
-                ),
-              ),
-            ],
-          ),
-        ),
+                ), r23
+              ),g56
+            ],vg2
+          ), ,nm1,56n ,56 46.kl6ç
+
+  5
+
+  k6p49p84l54o54564çl54ç5l46.f4 9.p[´dfçkrodkkok43k43k4koofki3p5]
+        ),h87
       ),
-    );
+    );qw5
   }
-}
+gj4}
+F45DF4D6F7456GR56T56R 564H 45R79DX8 / R]~F,.Ç~[.x5j56s jodjoçaJDO]i87j6gd56455
+5fvtxggf2
